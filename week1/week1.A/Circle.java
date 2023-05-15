fbc77584b4ddf05ef2d4ac340ec7357c037880ef
Arailym <78746109+Dautbekovna@users.noreply.github.com>
2023-05-15 16:09:32 +0600

    Circle.java

--- week1/week1.A/Circle.java
+++ week1/week1.A/Circle.java
@@ -1,31 +1,31 @@
 public class Circle {
-    private double radius;
+    private double r;
 
     public Circle() {
-        this.radius = 1.0;
+        this.r = 1.0;
     }
 
-    public Circle(double radius) {
-        this.radius = radius;
+    public Circle(double r) {
+        this.r = r;
     }
 
     public double getRadius() {
-        return this.radius;
+        return this.r;
     }
 
-    public void setRadius(double radius) {
-        this.radius = radius;
+    public void setRadius(double r) {
+        this.r = r;
     }
 
     public double getArea() {
-        return Math.PI * Math.pow(this.radius, 2);
+        return Math.PI * Math.pow(this.r, 2);
     }
 
     public double getCircumference() {
-        return Math.PI * 2 * this.radius;
+        return Math.PI * 2 * this.r;
     }
 
     public String toString() {
-        return "Circle[radius=" + this.radius + "]";
+        return "Circle[r = " + this.r + " ]";
     }
 }
